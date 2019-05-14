2bf292cd5439b5dec49b2d3cf8b2ea1157d28b3d
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-14 16:21:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function theBeatlesPlay(musicians, instruments){
 
 theBeatlesPlay(beatles, instruments)
 
-const facts = [ "He was the last Beatle to learn to drive",
+const fact = [ "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"];
@@ -37,4 +37,4 @@ function johnLennonFacts(facts){
   }
 }
 
-johnLennonFacts(facts)
+johnLennonFacts(fact)
