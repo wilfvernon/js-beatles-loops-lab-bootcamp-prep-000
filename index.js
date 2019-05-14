7223eba9680bae3c2b94420c637adece60f8fec7
Wilfred Alec Vernon <wilf.vernon@gmail.com>
2019-05-14 16:10:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// add solution here
+var beatles = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"]
+var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+var arr = []
+var n = 0
+var i = 0
+function theBeatlesPlay(musicians, instruments){
+  for (i = 0; i < 4; i++){
+      if (n === 4){
+      return arr
+    } else {
+    arr.push(musicians[n] + " plays " + instruments[n])
+    n++
+    }
+  }
+}
+
+const facts = [ "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"];
+  
+function johnLennonFacts(fact){
+  var clone = fact.slice(0)
+      while (clone.length > -1)
+       if (clone.length === 0) {
+         return arr
+       } else {
+     arr.push(clone[0]);
+     clone.push();
+     console.log(clone)
+  }
+}
+
+johnLennonFacts(facts)
